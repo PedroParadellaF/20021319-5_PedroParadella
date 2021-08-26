c38d7cf1b7b0743f26dc10a5737be67fe905d4bd
PedroParadellaF <pedroferreiraX@outlook.com>
2021-08-25 21:46:45 -0300

    iniciando trabalho

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta chardet="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="./css/main.css">
+        <title>Tabela valores</title>
+    </head>

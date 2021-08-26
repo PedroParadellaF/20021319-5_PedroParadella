e260dc0e544922213e4f0a0b4b75d85ffc57c9a3
PedroParadellaF <pedroferreiraX@outlook.com>
2021-08-25 22:42:18 -0300

    index.php alterada, main.css altrada, mudança de imagem

--- index.php
+++ index.php
@@ -11,8 +11,14 @@
         <link rel="stylesheet" type="text/css" href="./css/main.css">
         <title>Doces Caseiros | Home</title>
     </head>
+    <body>
+        <header>
+            <a href="#"><img src="images/logo.png" alt=""></a>
+        </header>
 
 
 
+
+    </body>
 
 </html>
